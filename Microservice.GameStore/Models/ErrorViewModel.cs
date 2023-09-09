ecfb0c40004044908ba6f6440f04f5f7c1af19f9
Daniil <daniil9360451@gmail.com>
2023-09-09 15:01:46 +0300

    Add project files.

---
+++ Microservice.GameStore/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Microservice.GameStore.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
